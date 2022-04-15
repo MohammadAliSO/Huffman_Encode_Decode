5f4454f00a3a64a8e9b6a1f9e4e065ff0fba5d01
MohammadAli.SO <MohammadAli.SO@DESKTOP-TQD5Q14>
2022-04-15 18:07:09 +0430

    ایجاد پروژه

---
+++ Huffman Coder Encoder/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Huffman_Coder_Encoder
+{
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("\t1)Compress File\n\r\t2)Decompress File");
+                Console.Write("Please enter number of Huffman mode:");
+                int mode = Convert.ToInt32(Console.ReadLine());
+
+
+
+                switch (mode)
+                {
+                    case 1:
+                        Console.Write("Please enter pathfile for compress:");
+                        var cfile=Console.ReadLine();
+                        HuffmanEncoder encoder = new HuffmanEncoder(cfile);
+                        var resultD =encoder.Encode();
+
+                        Console.WriteLine("File Compressed in :"+ resultD);
+                        break;
+                    case 2:
+                        Console.Write("Please enter pathfile for Decompress:");
+                        var dfile = Console.ReadLine();
+                        HuffmanDecoder Decoder = new HuffmanDecoder(dfile);
+                        var resultC = Decoder.Decode();
+
+                        Console.WriteLine("File Decompressed in :" + resultC);
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input!\a");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+        }
+
+    }
+
+}
